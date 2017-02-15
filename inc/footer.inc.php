aa324f6305bc7badb34e4d293a352ba92c81b932
TatianFischer <tatiana.fischer@hotmail.fr>
2017-02-15 08:06:44 +0100

    Passage en PHP
    Index, footer, header

---
+++ inc/footer.inc.php
@@ -0,0 +1,30 @@
+<!-- FOOTERs -->			
+			<footer class="bleu row" role="link">
+                <h3 class="col-sm-6"> Plus d'informations sur :</h3>
+                <ul class="col-sm-6" id="liens">
+                    <li class="col-xs-1 col-sm-offset-2">
+                        <a href="http://www.viadeo.com/fr/profile/tatiana.fischer1" target="_blank"><i class="fa fa-viadeo"></i></a>
+                    </li>
+                    <li class="col-xs-1 col-xs-offset-3">
+                        <a href="https://fr.linkedin.com/in/tatianafischer" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li class="col-xs-1 col-xs-offset-3">
+                        <a href="https://github.com/TatianFischer" target="_blank"><i class="fa fa-github"></i></a>
+                    </li>
+                </ul> <!-- fin des liens -->		
+            </footer>
+				
+            <footer>
+                <p id="copyright">Tous droits réservés @FISCHER Tatiana 2016-<?= date('Y'); ?></p>
+            </footer>
+<!-- Fin des FOOTERs -->
+    
+		</div><!-- Fin de la page -->
+		
+		<!-- jQuery -->
+        <script  src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+   
+        <!-- Latest compiled and minified JavaScript. http://getbootstrap.com/getting-started/ -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
-// Inclure le header
+// HEADER ------------------------------
+$title = 'Développeuse web';
+$page = 'Page d\'accueil du site';
+require_once('inc/header.inc.php');
 ?>
 <!-- Contenu HTML -->
-
+<main class="row r-m">
+    <div id="conteneur" class="col-xs-12">
+        <h1>Tatiana FISCHER <br> Développeur-Intégrateur Web Junior</h1>
+        <p>
+            Actuellement en formation intensive de développeur-Intégrateur à l'école WebForce3. Je suis prête à intégrer votre équipe en tant que <strong>développeuse full-stack</strong> dès le mois de <strong>mars 2017</strong>. <br>
+            Si vous êtes près à accueillir une fille dans votre équipe alors n'hésité pas à  <a href="contact">me contacter</a>.
+        </p>
+        <div id="bouton">
+            <a href="CV.php">
+                <img src="assets/images/curriculum.png" alt="vers le CV">
+            <p>Vers le CV</p>
+            </a>
+        </div>
+    </div><!-- fin du conteneur -->
+</main>
 <?php
-// Inclure le footer
+// FOOTER -------------------------------
+require_once('inc/footer.inc.php');
 ?>
